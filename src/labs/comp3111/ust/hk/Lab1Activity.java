1e9d8bddfa65da42a5bb8b01fdef746ed33a2218
WesleyNgai <WesleyNgai@10.89.249.73>
2018-02-13 11:23:57 +0800

    Implemented the sum of 10 integers

--- src/labs/comp3111/ust/hk/Lab1Activity.java
+++ src/labs/comp3111/ust/hk/Lab1Activity.java
@@ -3,7 +3,22 @@ package labs.comp3111.ust.hk;
 public class Lab1Activity {
 	public static void main(String[] args)
 	{
-
+		int[] arr={1,2,3,4,5,6,7,8,9,10};
+		int sum=0;
+//		int max=arr[0];
+//		int mini=arr[0];
+		for(int a:arr)
+		{
+			sum=sum+a;
+//			if(max<a)
+//				max=a;
+//			if(mini>a)
+//				mini=a;
+		}
+		
+		System.out.println(String.format("Sume of the numbers is %d", sum));
+//		System.out.print(String.format("Min=%d",mini));
+//		System.out.print(String.format("; Max=%d",max));
 	}
 
 }
